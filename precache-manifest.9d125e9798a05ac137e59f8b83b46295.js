811ffff9519660af9af9a51f1bc7303ef4c6c4d8
Jekib <jekib.haque@gmail.com>
2019-07-20 02:01:13 +0530

    Updates

---
+++ precache-manifest.9d125e9798a05ac137e59f8b83b46295.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a9d643b13e20f880b46c4bdb3fc8fd0",
+    "url": "/jekuuu/WeatherApp/index.html"
+  },
+  {
+    "revision": "e09bff9d313f7f60376d",
+    "url": "/jekuuu/WeatherApp/static/css/main.50a1293d.chunk.css"
+  },
+  {
+    "revision": "ec212e9eb06175f1ea1e",
+    "url": "/jekuuu/WeatherApp/static/js/2.5ed20b91.chunk.js"
+  },
+  {
+    "revision": "e09bff9d313f7f60376d",
+    "url": "/jekuuu/WeatherApp/static/js/main.51c709b6.chunk.js"
+  },
+  {
+    "revision": "4ebe352ce64384e2532e",
+    "url": "/jekuuu/WeatherApp/static/js/runtime~main.bab97ea4.js"
+  },
+  {
+    "revision": "e5fb8c5afa4cd7ae7621357f17dbda4a",
+    "url": "/jekuuu/WeatherApp/static/media/weather-app.e5fb8c5a.png"
+  }
+]);
