393ca19858458d8f75a97ae2f4db58aced40e6cb
Jekib <jekib.haque@gmail.com>
2019-07-20 02:07:17 +0530

    Updates

--- src/App.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import HomeContainer from "../src/containers/homeContainer";
-import "./index.css";
-
-function App() {
-  return <HomeContainer />;
-}
-
-export default App;
